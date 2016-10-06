8269d1533eb03b8c918123bf6b1fffcd039c8fcd
Joel Thibault <thibault@broadinstitute.org>
2016-10-06 13:43:03 -0400

    run comment

--- src/main/scala/poker/PokerApp.scala
+++ src/main/scala/poker/PokerApp.scala
@@ -1,6 +1,10 @@
 package poker
 
 object PokerApp {
+
+  // not very useful yet.  Just parses an input hand
+  // sbt "run src/main/resources/input1.txt"
+
   def main(args : Array[String]) {
     val cards = Parser.parse(args(0))
     cards foreach { c => println(c.toEnglish) }
